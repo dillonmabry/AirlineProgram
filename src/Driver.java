374e6126ee843ffcb466c416274ff09282520283
Dillon Mabry <dcmabry@gmail.com>
2016-02-25 17:41:00 -0500

    Updated plane and seat classes
    Added seat methods and final driver program

--- src/Driver.java
+++ src/Driver.java
@@ -2,6 +2,11 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Main Driver class
+ * @author dillonmabry
+ *
+ */
 public class Driver {
 
 	public static void main(String[] args) {
@@ -16,110 +21,345 @@ public class Driver {
 			System.out.println("---------------------------------------------");
 			try {
 				int choice = menu.nextInt();
-				if(choice < 0 || choice > 2) {
+				if (choice < 0 || choice > 2) {
 					System.out.println("Please enter menu choice between 0 and 2!");
 					continue;
 				}
 				switch (choice) {
-				
+
 				case 0:
 					Scanner input = new Scanner(System.in);
 					int classChoice, passChoice, pref;
 					System.out.print("Enter the class for passenger (0 for First, 1 for Economy): ");
-					classChoice = input.nextInt();			//class choice
-					if(classChoice < 0 || classChoice > 1) {
+					classChoice = input.nextInt(); // class choice
+					if (classChoice < 0 || classChoice > 1) {
 						System.out.println("Please enter a choice between 0 and 1! Returning to menu.");
 						break;
-					} else if (classChoice == 0) { //first class
+					} else if (classChoice == 0) { // first class
 						System.out.print("Please enter number of passengers between 1 and 2: ");
-						passChoice = input.nextInt();			//num passengers
-						if(passChoice < 1 || passChoice > 2) {
+						passChoice = input.nextInt(); // num passengers
+						if (passChoice < 1 || passChoice > 2) {
 							System.out.println("Please enter a choice between 1 and 2! Returning to menu.");
 							break;
 						} else if (passChoice == 1) {
-							System.out.print("Please enter seating preferences (0 for window, 1 for aisle): "); //add pref for 1 
+							System.out.print("Please enter seating preferences (0 for window, 1 for aisle): "); // 1
 							pref = input.nextInt();
-							if(pref < 0 || pref > 1) {
+							if (pref < 0 || pref > 1) {
 								System.out.println("Please enter a choice between 0 and 1! Returning to menu.");
 								break;
-							} else if(pref == 0) {
-								for(int i=0; i < plane.firstClass.size(); i++) {
-									if(plane.firstClass.get(i).getStatus() == false && plane.firstClass.get(i).getSeatPref().equals("window")) {
-										plane.firstClassAddSeat(String.valueOf(classChoice),passChoice,"window",plane.firstClass.get(i));
+							} else if (pref == 0) {
+								for (int i = 0; i < plane.firstClass.size(); i++) {
+									if (plane.firstClass.get(i).getStatus() == false
+											&& plane.firstClass.get(i).getSeatPref().equals("window")) {
+										plane.firstClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+												plane.firstClass.get(i));
 										break;
 									} else {
-										System.out.println("Moving passenger to next available seat.");
+										System.out.println("Seat not available!");
+										continue;
 									}
 								}
-								
+
 							} else {
-								for(int i=0; i < plane.firstClass.size(); i++) {
-									if(plane.firstClass.get(i).getStatus() == false && plane.firstClass.get(i).getSeatPref().equals("aisle")) {
-										plane.firstClassAddSeat(String.valueOf(classChoice),passChoice,"aisle",plane.firstClass.get(i));
+								for (int i = 0; i < plane.firstClass.size(); i++) {
+									if (plane.firstClass.get(i).getStatus() == false
+											&& plane.firstClass.get(i).getSeatPref().equals("aisle")) {
+										plane.firstClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+												plane.firstClass.get(i));
 										break;
 									} else {
-										System.out.println("Moving passenger to next available seat.");
+										System.out.println("Seat not available!");
+										continue;
 									}
 								}
 							}
-								
-						} else {	//add pref for 2
-							System.out.print("Please enter seating preferences (0 for window, 1 for aisle): "); //add pref for 1 
+
+						} else { // add pref for 2
+							System.out.print("Please enter seating preferences (0 for window, 1 for aisle): "); // 1
 							pref = input.nextInt();
-							if(pref < 0 || pref > 1) {
+							if (pref < 0 || pref > 1) {
 								System.out.println("Please enter a choice between 0 and 1! Returning to menu.");
 								break;
-							} else if(pref == 0) {
-								for(int i=0; i < plane.firstClass.size(); i++) {
-									if(plane.firstClass.get(i).getStatus() == false && plane.firstClass.get(i).getSeatPref().equals("window")) {
-										plane.firstClassAddSeat(String.valueOf(classChoice),passChoice,"window",plane.firstClass.get(i));
-										if(plane.firstClass.get(i+2).getStatus()==false) {
-											plane.firstClassAddSeat(String.valueOf(classChoice),passChoice,"window",plane.firstClass.get(i+2));
+							} else if (pref == 0) {
+								for (int i = 0; i < plane.firstClass.size(); i++) {
+									if (plane.firstClass.get(i).getStatus() == false
+											&& plane.firstClass.get(i).getSeatPref().equals("window")) {
+										plane.firstClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+												plane.firstClass.get(i));
+										if (plane.firstClass.get(i + 2).getStatus() == false
+												&& plane.firstClass.get(i + 2).getSeatPref().equals("window")) {
+											plane.firstClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+													plane.firstClass.get(i + 2));
 										} else {
-											System.out.println("Moving passenger to next available seat.");
+											System.out.println("Seat not available!");
+											continue;
 										}
 										break;
 									} else {
-										System.out.println("Moving passenger to next available seat.");
+										System.out.println("Seat not available!");
+										continue;
 									}
 								}
-								
+
 							} else {
-								for(int i=0; i < plane.firstClass.size(); i++) {
-									if(plane.firstClass.get(i).getStatus() == false && plane.firstClass.get(i).getSeatPref().equals("aisle")) {
-										plane.firstClassAddSeat(String.valueOf(classChoice),passChoice,"aisle",plane.firstClass.get(i));
-										if(plane.firstClass.get(i+2).getStatus()==false) {
-											plane.firstClassAddSeat(String.valueOf(classChoice),passChoice,"aisle",plane.firstClass.get(i+2));
+								for (int i = 0; i < plane.firstClass.size(); i++) {
+									if (plane.firstClass.get(i).getStatus() == false
+											&& plane.firstClass.get(i).getSeatPref().equals("aisle")) {
+										plane.firstClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+												plane.firstClass.get(i));
+										if (plane.firstClass.get(i + 2).getStatus() == false
+												&& plane.firstClass.get(i + 2).getSeatPref().equals("aisle")) {
+											plane.firstClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+													plane.firstClass.get(i + 2));
 										} else {
-											System.out.println("Moving passenger to next available seat.");
+											System.out.println("Seat not available!");
+											continue;
 										}
 										break;
 									} else {
-										System.out.println("Moving passenger to next available seat.");
+										System.out.println("Seat not available!");
+										continue;
 									}
 								}
 							}
-							
-							
+
 						}
-//						plane.firstClassAddSeat(num, seatClass, numPass, pref);
-					} else { //economy class
-						
-					}
-					
-					
-					
-					
+					} else { // economy
+						System.out.print("Please enter number of passengers between 1 and 3: ");
+						passChoice = input.nextInt(); // num passengers
+						if (passChoice < 1 || passChoice > 3) {
+							System.out.println("Please enter a choice between 1 and 3! Returning to menu.");
+							break;
+						} else if (passChoice == 1) {
+							System.out.print(
+									"Please enter seating preferences (0 for window, 1 for center, 2 for aisle): "); // add
+																														// pref
+																														// for
+																														// 1
+							pref = input.nextInt();
+							if (pref < 0 || pref > 2) {
+								System.out.println("Please enter a choice between 0 and 1! Returning to menu.");
+								break;
+							} else if (pref == 0) { // seat for window
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i).getStatus() == false
+											&& plane.economyClass.get(i).getSeatPref().equals("window")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+												plane.economyClass.get(i));
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+
+							} else if (pref == 1) { // seating for center
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i).getStatus() == false
+											&& plane.economyClass.get(i).getSeatPref().equals("center")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "center",
+												plane.economyClass.get(i));
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+							} else { // seating for aisle
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i).getStatus() == false
+											&& plane.economyClass.get(i).getSeatPref().equals("aisle")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+												plane.economyClass.get(i));
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+							}
+
+						} else if (passChoice == 2) { // add pref for 2
+							System.out.print(
+									"Please enter seating preferences (0 for window, 1 for center, 2 for aisle): ");
+							pref = input.nextInt();
+							if (pref < 0 || pref > 2) {
+								System.out.println("Please enter a choice between 0 and 1! Returning to menu.");
+								break;
+							} else if (pref == 0) { // seat for window
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i).getStatus() == false
+											&& plane.economyClass.get(i).getSeatPref().equals("window")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+												plane.economyClass.get(i));
+										if (plane.economyClass.get(i + 3).getStatus() == false
+												&& plane.economyClass.get(i + 3).getSeatPref().equals("window")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+													plane.economyClass.get(i + 3));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+
+							} else if (pref == 1) { // seating for center
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i + 1).getStatus() == false
+											&& plane.economyClass.get(i + 1).getSeatPref().equals("center")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "center",
+												plane.economyClass.get(i + 1));
+										if (plane.economyClass.get(i + 4).getStatus() == false
+												&& plane.economyClass.get(i + 4).getSeatPref().equals("center")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "center",
+													plane.economyClass.get(i + 4));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+							} else { // seating for aisle
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i + 2).getStatus() == false
+											&& plane.economyClass.get(i + 2).getSeatPref().equals("aisle")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+												plane.economyClass.get(i + 2));
+										if (plane.economyClass.get(i + 5).getStatus() == false
+												&& plane.economyClass.get(i + 5).getSeatPref().equals("aisle")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+													plane.economyClass.get(i + 5));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+							}
+
+						} else { // choice for 3 passengers
+							System.out.print(
+									"Please enter seating preferences (0 for window, 1 for center, 2 for aisle): "); // add
+																														// pref
+																														// for
+																														// 1
+							pref = input.nextInt();
+							if (pref < 0 || pref > 2) {
+								System.out.println("Please enter a choice between 0 and 1! Returning to menu.");
+								break;
+							} else if (pref == 0) { // seat for window
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i).getStatus() == false
+											&& plane.economyClass.get(i).getSeatPref().equals("window")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+												plane.economyClass.get(i));
+										if (plane.economyClass.get(i + 3).getStatus() == false
+												&& plane.economyClass.get(i + 3).getSeatPref().equals("window")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+													plane.economyClass.get(i + 3));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										if (plane.economyClass.get(i + 6).getStatus() == false
+												&& plane.economyClass.get(i + 6).getSeatPref().equals("window")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "window",
+													plane.economyClass.get(i + 6));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+
+							} else if (pref == 1) { // seating for center
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i + 1).getStatus() == false
+											&& plane.economyClass.get(i + 1).getSeatPref().equals("center")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "center",
+												plane.economyClass.get(i + 1));
+										if (plane.economyClass.get(i + 4).getStatus() == false
+												&& plane.economyClass.get(i + 4).getSeatPref().equals("center")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "center",
+													plane.economyClass.get(i + 4));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										if (plane.economyClass.get(i + 7).getStatus() == false
+												&& plane.economyClass.get(i + 7).getSeatPref().equals("center")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "center",
+													plane.economyClass.get(i + 7));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+							} else { // seating for aisle
+								for (int i = 0; i < plane.economyClass.size(); i++) {
+									if (plane.economyClass.get(i + 2).getStatus() == false
+											&& plane.economyClass.get(i + 2).getSeatPref().equals("aisle")) {
+										plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+												plane.economyClass.get(i + 2));
+										if (plane.economyClass.get(i + 5).getStatus() == false
+												&& plane.economyClass.get(i + 5).getSeatPref().equals("aisle")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+													plane.economyClass.get(i + 5));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										if (plane.economyClass.get(i + 8).getStatus() == false
+												&& plane.economyClass.get(i + 8).getSeatPref().equals("aisle")) {
+											plane.economyClassAddSeat(String.valueOf(classChoice), passChoice, "aisle",
+													plane.economyClass.get(i + 8));
+										} else {
+											System.out.println("Seat not available!");
+											continue;
+										}
+										break;
+									} else {
+										System.out.println("Seat not available!");
+										continue;
+									}
+								}
+							}
+						}
+
+					} // end of economy
+
 					break;
-				case 1:
+				case 1: // print seating
 					plane.printFirstClassSeats();
 					System.out.println();
 					System.out.println("-----End of First Class Seats-----");
 					System.out.println();
 					plane.printEconomyClassSeats();
 					break;
-				case 2:
+				case 2: // exit
 					System.out.println("Exiting application!");
+					menu.close();
 					System.exit(0);
 					break;
 				}
@@ -128,19 +368,8 @@ public class Driver {
 				menu.next();
 				continue;
 			}
+		} // end of while
 
-		}
-
-		// airplane plane = new airplane();
-		// System.out.println(plane.economyClass.get(89).getStatus());
-		// plane.addSeat(0,"first", 1, "aisle");
-
-		// plane.firstClass.get(1).setStatus(true);
-		// System.out.println(plane.firstClass.get(1).getStatus());
-		// plane.addSeat(1,"first", 1, "aisle");
-		// plane.firstClass.get(0).setStatus(true);
-		// plane.addSeat(2,"first", 1, "aisle");
-		// plane.printSeats();
-	}
+	} // end of main
 
 }
--- src/airplane.java
+++ src/airplane.java
@@ -3,108 +3,132 @@ import java.util.ArrayList;
 
 public class airplane {
 
-	ArrayList<Seat> firstClass = new ArrayList<Seat>();
-	ArrayList<Seat> economyClass = new ArrayList<Seat>();
-	
+	protected ArrayList<Seat> firstClass = new ArrayList<Seat>();
+	protected ArrayList<Seat> economyClass = new ArrayList<Seat>();
+
+	/**
+	 * Constructor to create airplane object with new Seat objects
+	 * Creates 20 first class seats with window/aisle seats and
+	 * 90 economy class seat with window/center/aisle seats
+	 */
 	public airplane() {
 		int num = 1;
-		for(int i=0; i< 20; i++) {
-			if(num%2 != 0) {
-				firstClass.add(new Seat(num,"",0,"window"));
+		for (int i = 0; i < 20; i++) {
+			if (num % 2 != 0) {
+				firstClass.add(new Seat(num, "", 0, "window"));
 			} else {
-				firstClass.add(new Seat(num,"",0,"aisle"));
+				firstClass.add(new Seat(num, "", 0, "aisle"));
 			}
 			num++;
 		}
 		int count = 1;
-		for(int j=0; j < 90; j++) {
-			if(count%3 ==0) {
-				economyClass.add(new Seat(count,"",0,"aisle"));
-			} else if(count%3 == 1) {
-				economyClass.add(new Seat(count,"",0,"window"));
+		for (int j = 0; j < 90; j++) {
+			if (count % 3 == 0) {
+				economyClass.add(new Seat(count, "", 0, "aisle"));
+			} else if (count % 3 == 1) {
+				economyClass.add(new Seat(count, "", 0, "window"));
 			} else {
-				economyClass.add(new Seat(count,"",0,"center"));
+				economyClass.add(new Seat(count, "", 0, "center"));
 			}
 			count++;
 		}
 	}
-	
+
+	/**
+	 * Method to add a first class seat to the airplane
+	 * Modifies the current existing Seat and updates status
+	 * @param seatClass the class of seat
+	 * @param numPass the number of passengers
+	 * @param pref the preference of location
+	 * @param seat the Seat to update
+	 */
 	public void firstClassAddSeat(String seatClass, int numPass, String pref, Seat seat) {
-		
 		seat.setStatus(true);
-		seat.seatClass = seatClass;
-		seat.numPassengers = numPass;
-		seat.seatPref = pref;
-		
-//		if(seatClass.toLowerCase().equals("first")) {
-//			
-//			if(numPass < 1 || numPass > 2) {
-//				System.out.println("Error please enter between 1 and 2 passengers for first class!");
-//				return;	
-//			} 
-//			
-//			if(!(pref.toLowerCase().equals("window")|| pref.toLowerCase().equals("aisle"))) {
-//				System.out.println("Error please enter window or aisle for seat preferences!");
-//				return;
-//			}
-//			
-//			firstClass.add(new Seat(num,seatClass,numPass,pref));
-//			
-//		} else if (seatClass.toLowerCase().equals("economy")) {
-//			economyClass.add(new Seat(num, seatClass,numPass,pref));
-//		}
+		seat.setClass(seatClass);
+		seat.setNumPass(numPass);
+		seat.setSeatPref(pref);
 	}
-	
-	public void economyClassAddSeat(int num, String seatClass, int numPass, String pref) {
-		economyClass.add(new Seat(num,seatClass,numPass,pref));
+
+	/**
+	 * Method to add an economy class seat to the airplane
+	 * Modifies the current existing Seat and updates status
+	 * @param seatClass the class of seat
+	 * @param numPass the number of passengers
+	 * @param pref the preference of location
+	 * @param seat the Seat to update
+	 */
+	public void economyClassAddSeat(String seatClass, int numPass, String pref, Seat seat) {
+		seat.setStatus(true);
+		seat.setClass(seatClass);
+		seat.setNumPass(numPass);
+		seat.setSeatPref(pref);
 	}
-	
+
+	/**
+	 * Method to print out the first class seating arrangement
+	 * from top left to bottom right for left side.
+	 * Right side prints top right to bottom left.
+	 */
 	public void printFirstClassSeats() {
 		System.out.println("Left Side of Plane (from TOP LEFT to BOTTOM RIGHT): \n");
-		for(int i=0; i< firstClass.size()/2; i++) {
-			System.out.print("Seat class: First | "+"Seat number: "+firstClass.get(i).seatNum+" | "+
-					" "+"taken: "+firstClass.get(i).isTaken+" | Seat loc: "+firstClass.get(i).seatPref);
+		for (int i = 0; i < firstClass.size() / 2; i++) {
+			System.out.print("Seat class: First | " + "Seat number: " + firstClass.get(i).getNumber() + " | " + " "
+					+ "taken: " + firstClass.get(i).getStatus() + " | Seat loc: " + firstClass.get(i).getSeatPref());
 			System.out.println();
 		}
 		System.out.println();
 		System.out.println("Right side of Plane (from TOP RIGHT TO BOTTOM LEFT): \n");
-		for(int i=10; i< firstClass.size(); i++) {
-			System.out.print("Seat class: First | "+"Seat number: "+firstClass.get(i).seatNum+" | "+
-					" "+"taken: "+firstClass.get(i).isTaken+" | Seat loc: "+firstClass.get(i).seatPref);
+		for (int i = 10; i < firstClass.size(); i++) {
+			System.out.print("Seat class: First | " + "Seat number: " + firstClass.get(i).getNumber() + " | " + " "
+					+ "taken: " + firstClass.get(i).getStatus() + " | Seat loc: " + firstClass.get(i).getSeatPref());
 			System.out.println();
 		}
-		
+
 	}
-	
+
+	/**
+	 * Method to print out the economy class seating arrangement
+	 * from top left to bottom right for left side.
+	 * Right side prints top right to bottom left.
+	 */
 	public void printEconomyClassSeats() {
-//		for(int i=0; i< economyClass.size(); i++) {
-//			System.out.print("Seat class: Economy | "+"Seat number: "+economyClass.get(i).seatNum+" | "+
-//					" "+"taken: "+economyClass.get(i).isTaken+"Seat loc: "+economyClass.get(i).seatPref);
-//			System.out.println();
-//		}
 		System.out.println("Left Side of Plane (from TOP LEFT to BOTTOM RIGHT): \n");
-		for(int i=0; i< economyClass.size()/2; i++) {
-			System.out.print("Seat class: Economy | "+"Seat number: "+economyClass.get(i).seatNum+" | "+
-					" "+"taken: "+economyClass.get(i).isTaken+" | Seat loc: "+economyClass.get(i).seatPref);
+		for (int i = 0; i < economyClass.size() / 2; i++) {
+			System.out.print("Seat class: Economy | " + "Seat number: " + economyClass.get(i).getNumber() + " | " + " "
+					+ "taken: " + economyClass.get(i).getStatus() + " | Seat loc: "
+					+ economyClass.get(i).getSeatPref());
 			System.out.println();
 		}
 		System.out.println();
 		System.out.println("Right side of Plane (from TOP RIGHT TO BOTTOM LEFT): \n");
-		for(int i=45; i< economyClass.size(); i++) {
-			System.out.print("Seat class: Economy | "+"Seat number: "+economyClass.get(i).seatNum+" | "+
-					" "+"taken: "+economyClass.get(i).isTaken+" | Seat loc: "+economyClass.get(i).seatPref);
+		for (int i = 45; i < economyClass.size(); i++) {
+			System.out.print("Seat class: Economy | " + "Seat number: " + economyClass.get(i).getNumber() + " | " + " "
+					+ "taken: " + economyClass.get(i).getStatus() + " | Seat loc: "
+					+ economyClass.get(i).getSeatPref());
 			System.out.println();
 		}
 	}
 
-	public class Seat {
+	/**
+	 * Class for creating Seats for the airplane Class
+	 * @author dillonmabry
+	 *
+	 */
+	protected class Seat {
 		private int seatNum;
 		private String seatClass;
 		private int numPassengers;
 		private String seatPref;
 		private boolean isTaken;
-		
-		public Seat(int seatNum,String seatClass, int numPass, String seatPref) {
+
+		/**
+		 * Constructor for creating a seat
+		 * @param seatNum the seat number
+		 * @param seatClass the class assigned to the seat
+		 * @param numPass number of passengers
+		 * @param seatPref the preference location
+		 */
+		public Seat(int seatNum, String seatClass, int numPass, String seatPref) {
 			this.seatNum = seatNum;
 			this.seatClass = seatClass;
 			this.numPassengers = numPass;
@@ -112,26 +136,62 @@ public class airplane {
 			this.isTaken = false;
 		}
 
-		public void setStatus(boolean status) {
+		/**
+		 * Setter to set seat status
+		 * @param status whether the seat is available
+		 */
+		public void setStatus(boolean status) { // set seat status
 			this.isTaken = status;
 		}
-		
-		public boolean getStatus() {
+
+		/** 
+		 * Getter to get the status of the seat
+		 * @return the seat status
+		 */
+		public boolean getStatus() { // get seat status
 			return this.isTaken;
 		}
-		
-		public int getNumber() {
+
+		/**
+		 * Getter to get the seat number
+		 * @return the seat number
+		 */
+		public int getNumber() { // get seat number
 			return this.seatNum;
 		}
-		
-		public String getSeatPref() {
+
+		/**
+		 * Getter to get the preference location
+		 * @return seat location
+		 */
+		public String getSeatPref() { // get pref
 			return this.seatPref;
 		}
-		
-		public void setSeatPref(String pref) {
+
+		/**
+		 * Setter to seat seat preference
+		 * @param pref the location preference 
+		 */
+		public void setSeatPref(String pref) { // set pref
 			this.seatPref = pref;
 		}
-		
-	} //end of Seat class
-	
-} //end of airplane class
+
+		/**
+		 * Setter to set the number of passengers
+		 * @param pass the number of passengers
+		 */
+		public void setNumPass(int pass) { // set number of passengers
+			this.numPassengers = pass;
+		}
+
+		/**
+		 * Setter to set the class of the seat
+		 * @param sClass the class of the seat to set
+		 */
+		public void setClass(String sClass) { // set seat class
+			this.seatClass = sClass;
+		}
+
+	} // end of Seat class
+
+} // end of airplane class
